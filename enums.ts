92dbd419faab1c0a330a7e9db286d5b2fb77f742
DevRizel <rbracero27@gmail.com>
2024-11-05 19:43:37 +0800

    Create enums.ts

---
+++ enums.ts
@@ -0,0 +1,16 @@
+export enum Status {
+  Todo = "To Do",
+  InProgress = "In Progress",
+  Done = "Done",
+}
+
+export enum Priority {
+  Low = "Low",
+  Medium = "Medium",
+  High = "High",
+}
+
+export enum NotificationType {
+  TaskAssigned = "Task Assigned",
+  TaskUpdated = "Task Updated",
+}
